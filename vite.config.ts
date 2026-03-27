09aa14defa46d177690deb590204de0116fa9458
Arsene <80160115+Fleurishment@users.noreply.github.com>
2026-03-27 12:33:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/krneldb/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
